1363886c485f24d84728e8e7fd0ffe0106de4ca9
Eugene Correia <ecorreia@blueskybroadcast.com>
2014-08-19 21:29:05 -0700

    First commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in omniauth-i4a.gemspec
+gemspec
---
+++ lib/omniauth/i4a.rb
@@ -0,0 +1,7 @@
+require "omniauth/i4a/version"
+
+module Omniauth
+  module I4a
+    # Your code goes here...
+  end
+end
